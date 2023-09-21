b62bb53d9b51764ad2f85f26fd684733e28b83ac
charu123123 <143795990+charu123123@users.noreply.github.com>
2023-09-21 12:27:00 +0530

    Add files via upload

---
+++ Sumof3numbers.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class Sumof3numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=20;
+int y=30;
+int z=30;
+int c=x+y+z;
+System.out.println(c);
+	}
+
+}
