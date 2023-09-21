1d47a875caf008d6f1c40704608dd788d3261796
charu123123 <143795990+charu123123@users.noreply.github.com>
2023-09-21 12:08:12 +0530

    Add files via upload

---
+++ CubeOfNumbers.java
@@ -0,0 +1,23 @@
+package assignment2;
+import java.util.Scanner;
+
+public class CubeOfNumbers {
+
+	public static void main(String[] args) {
+		int n,i;
+		System.out.print("Enter the last number for cube : ");
+		Scanner Sc= new Scanner(System.in);
+
+		
+		n = Sc.nextInt();
+
+		 
+		for(i=1;i<=n;i++)
+		{
+			System.out.println("Cube of " +i+" is : "+(i*i*i));     
+		}
+		}
+		
+	}
+
+
