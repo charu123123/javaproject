cf98238be99b085b8ce34e895524d826b68af053
charu123123 <143795990+charu123123@users.noreply.github.com>
2023-09-21 12:27:26 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,53 @@
+package chapter1;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=20;
+int b=15;
+int add=a+b;
+System.out.println(add);
+int substraction=a-b;
+System.out.println(substraction);
+int multiply=a*b;
+System.out.println(multiply);
+float division=a/b;
+System.out.println(division);
+float remainder=a%b;
+System.out.println(remainder);
+a += b;
+System.out.println("a="+a);
+a -= b;
+System.out.println("a="+a);
+a*=b;
+System.out.println("a="+a);
+a/=b;
+System.out.println("a="+a);
+a%=b;
+System.out.println("a="+a);
+System.out.println("a>b:"+(a>b));
+System.out.println("a<b:"+(a<b));
+System.out.println("a==b:"+(a==b));
+boolean x=true;
+boolean y=false;
+System.out.println("x && y:"+(x && y));
+System.out.println("x || y:"+(x || y));
+
+System.out.println("!x:"+(!x));
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
