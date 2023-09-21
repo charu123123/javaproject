67dfb17550a8eaa12322d1fef81c68579211d922
charu123123 <143795990+charu123123@users.noreply.github.com>
2023-09-21 12:26:35 +0530

    Add files via upload

---
+++ Kilomilesconversion.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class Kilomilesconversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double kilometer=5;
+ 
+double miles=kilometer*0.621;
+System.out.println(miles+"miles");
+
+	}
+
+}
